f15aadc2a8159ae311bdff1eb1d30c4c31bdf6d2
Vincent Liu <vincentliu1417@gmail.com>
2024-07-01 16:07:57 -0400

    finished adding all pages
    database still not working

---
+++ bookstore/src/Homepage.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="Homepage" style={{width: 1440, height: 3834, position: 'relative', background: '#FDFBE4'}}>
+      <img className="Homepageimage" style={{width: 1441, height: 1110, left: 0, top: 206, position: 'absolute'}} src="https://via.placeholder.com/1441x1110" alt="Homepage" />
+      <div className="Team3Bookstore" style={{width: 378, height: 122, left: 531, top: 378, position: 'absolute', textAlign: 'center', color: '#A35950', fontSize: 48, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>TEAM 3<br />BOOKSTORE</div>
+      <div className="Bottomdivider" style={{width: 1440, height: 6, left: 0, top: 1313, position: 'absolute', background: '#A35950'}} />
+      <div className="Topdivider" style={{width: 1440, height: 6, left: 0, top: 200, position: 'absolute', background: '#A35950'}} />
+      <div className="Rectangle5" style={{width: 1214, height: 6, left: 114, top: 1545, position: 'absolute', background: '#A35950'}} />
+      <div className="Featuredsection" style={{width: 1203, height: 532, left: 125, top: 1594, position: 'absolute'}}>
+        <div className="FeaturedBooks" style={{left: 396, top: 0, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 64, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>Featured Books</div>
+        <div className="Featuredbook1" style={{width: 214, height: 421, left: 0, top: 111, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 11, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <img className="Image1featuredbook" style={{width: 163, height: 246, left: 26, top: 0, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/163x246" alt="Featured Book 1" />
+          <div className="ToKillAMockingbirdHarperLee1899Paperback" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>TO KILL A MOCKINGBIRD<br />HARPER LEE<br />$18.99 PAPERBACK</div>
+          <div className="AddToCart" style={{left: 32, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 158, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" alt="Cart Icon" />
+        </div>
+        <div className="Featuredbook2" style={{width: 193, height: 421, left: 334, top: 111, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 0, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 21, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 147, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" alt="Cart Icon" />
+          <div className="TheHobbitJRRTolkien2299Paperback" style={{left: 14, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>THE HOBBIT<br />J.R.R TOLKIEN<br />$22.99 PAPERBACK</div>
+          <img className="Image2featuredbook" style={{width: 167, height: 258, left: 13, top: 0, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/167x258" alt="Featured Book 2" />
+        </div>
+        <div className="Featuredbook3" style={{width: 203, height: 421, left: 652, top: 111, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 5, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 26, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 152, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" alt="Cart Icon" />
+          <div className="JourneyToTheStarsMattZhang999Hardcover" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>JOURNEY TO THE STARS<br />MATT ZHANG<br />$9.99 HARDCOVER</div>
+          <img className="Image3featuredbook" style={{width: 162, height: 258, left: 20, top: 0, position: 'absolute'}} src="https://via.placeholder.com/162x258" alt="Featured Book 3" />
+        </div>
+        <div className="Featuredbook4" style={{width: 253, height: 421, left: 950, top: 111, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 30, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 51, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 177, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" alt="Cart Icon" />
+          <div className="TheAdventureOfGrizzlieAndrewWilson1299Paperback" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: '35px', wordWrap: 'break-word'}}>THE ADVENTURE OF GRIZZLIE<br />ANDREW WILSON<br />$12.99 PAPERBACK</div>
+          <img className="Image4featuredbook" style={{width: 161, height: 258, left: 45, top: 0, position: 'absolute'}} src="https://via.placeholder.com/161x258" alt="Featured Book 4" />
+        </div>
+      </div>
+      <div className="Comingsoonsection" style={{width: 1203, height: 549, left: 119, top: 2703, position: 'absolute'}}>
+        <div className="ComingSoon" style={{left: 428, top: 0, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 64, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>Coming Soon</div>
+        <div className="Comingsoonbook1" style={{width: 214, height: 421, left: 0, top: 128, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 17, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 38, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 164, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" />
+          <img className="Image1comingsoon" style={{width: 163, height: 246, left: 26, top: 0, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/163x246" />
+          <div className="ToKillAMockingbirdHarperLee1899Paperback" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>TO KILL A MOCKINGBIRD<br/>HARPER LEE<br/>$18.99 PAPERBACK</div>
+        </div>
+        <div className="Comingsoonbook2" style={{width: 193, height: 421, left: 340, top: 128, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 0, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 21, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 147, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" />
+          <div className="TheHobbitJRRTolkien2299Paperback" style={{left: 8, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>THE HOBBIT<br/>J.R.R TOLKIEN<br/>$22.99 PAPERBACK</div>
+          <img className="Image2comingsoon" style={{width: 167, height: 258, left: 7, top: 0, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/167x258" />
+        </div>
+        <div className="Comingsoonbook3" style={{width: 204, height: 421, left: 652, top: 128, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 11, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 32, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 158, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" />
+          <div className="JourneyToTheStarsMattZhang999Hardcover" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>JOURNEY TO THE STARS<br/>MATT ZHANG<br/>$9.99 HARDCOVER</div>
+          <img className="Image3comingsoon" style={{width: 162, height: 258, left: 20, top: 0, position: 'absolute'}} src="https://via.placeholder.com/162x258" />
+        </div>
+        <div className="Comingsoonbook4" style={{width: 253, height: 421, left: 950, top: 128, position: 'absolute'}}>
+          <div className="Button" style={{width: 193, height: 42, left: 36, top: 379, position: 'absolute', background: '#A35950', borderRadius: 30}} />
+          <div className="AddToCart" style={{left: 57, top: 382, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>ADD TO CART</div>
+          <img className="Carticon" style={{width: 25, height: 22, left: 183, top: 389, position: 'absolute'}} src="https://via.placeholder.com/25x22" />
+          <div className="TheAdventureOfGrizzlieAndrewWilson1299Paperback" style={{left: 0, top: 258, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Gaegu', fontWeight: '700', lineHeight: 35, wordWrap: 'break-word'}}>THE ADVENTURE OF GRIZZLIE<br/>ANDREW WILSON<br/>$12.99 PAPERBACK</div>
+          <img className="Image4comingsoon" style={{width: 161, height: 258, left: 45, top: 0, position: 'absolute'}} src="https://via.placeholder.com/161x258" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
